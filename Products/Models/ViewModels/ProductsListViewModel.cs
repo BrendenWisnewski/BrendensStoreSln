e8da7fca08be4c8649a5dd57e8c01f96d4f60534
brendenwisnewski <brendenwisnewski@DESKTOP-NKRPF7U>
2021-03-25 20:49:31 -0700

    Committing all final changes

---
+++ Products/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Products.Models;
+
+namespace Products.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
